d753743ac2c333aa851ec0e0dd5e436b7c9552e4
Chris <christopheriancaudill@gmail.com>
2025-01-21 20:43:18 -0500

    add basic chirps index template

--- chirps/views.py
+++ chirps/views.py
@@ -1,10 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader
+from .models import Chirp
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the chirps index.")
+    latest_chirp_list = Chirp.objects.order_by("-created_at")
+    template = loader.get_template("index.html")
+    context = {
+        "latest_chirp_list": latest_chirp_list,
+    }
+    return HttpResponse(template.render(context, request))
 
 def edit(request, chirp_id):
     return HttpResponse("Hello, world. You're at the chirps edit index.")
